f944bbcab438f41e0b9a935f6c200e775432503a
Plac3hold3r <Plac3hold3r+github@outlook.com>
2016-12-22 18:42:21 +0200

    Add project files.

---
+++ TipCalc.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,70 @@
+﻿
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Plugins.Messenger;
+using TipCalc.Core.Services;
+
+namespace TipCalc.Core.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        readonly ICalculation _calculation;
+        readonly IMvxMessenger _mvxMessenger;
+
+        public TipViewModel(ICalculation calculation, IMvxMessenger mvxMessenger)
+        {
+            _calculation = calculation;
+            _mvxMessenger = mvxMessenger;
+        }
+
+        public override void Start()
+        {
+            _subTotal = 100;
+            _generosity = 10;
+            Recalcuate();
+            base.Start();
+        }
+
+        double _subTotal;
+
+        public double SubTotal
+        {
+            get { return _subTotal; }
+            set
+            {
+                _subTotal = value;
+                RaisePropertyChanged(() => SubTotal);
+                Recalcuate();
+            }
+        }
+
+        int _generosity;
+
+        public int Generosity
+        {
+            get { return _generosity; }
+            set
+            {
+                _generosity = value;
+                RaisePropertyChanged(() => Generosity);
+                Recalcuate();
+            }
+        }
+
+        double _tip;
+
+        public double Tip
+        {
+            get { return _tip; }
+            set
+            {
+                _tip = value;
+                RaisePropertyChanged(() => Tip);
+            }
+        }
+
+        void Recalcuate()
+        {
+            Tip = _calculation.TipAmount(SubTotal, Generosity);
+        }
+    }
+}
---
+++ TipCalc.UI.Droid/Bootstrap/MessengerPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace TipCalc.UI.Droid.Bootstrap
+{
+    public class MessengerPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Messenger.PluginLoader>
+    {
+    }
+}
